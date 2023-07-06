799ce374740c0c619d7fb35c99136efa1471310d
kcat2201 <kcat2201@gmail.com>
2023-07-06 23:30:37 +0900

    Update #3 - Main
    변수 클래스를 인터페이스로 변경
    #3 은 여기서 마무리.
    스프링으로 넘어감

--- src/main/java/org/coba/Main.java
+++ src/main/java/org/coba/Main.java
@@ -1,11 +1,13 @@
 package org.coba;
 
-import org.coba.logic.BubbleSort;
+import org.coba.logic.JavaSort;
+import org.coba.logic.Sort;
+
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        BubbleSort<String> sort = new BubbleSort<>();
+        Sort<String> sort = new JavaSort<>(); //변수선언부를 바꾸지않고, 구현체만 바꾸는게 가능해짐
 
         System.out.println("[result] " + sort.sort(Arrays.asList(args)));
     }
